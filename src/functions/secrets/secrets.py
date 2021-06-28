24c40e7bb6cc31ad8a574f29fca7c3621484700d
falkzeh <mail@falkzeh.io>
2021-06-28 21:41:55 +0100

    update secrets to env variables

---
+++ src/functions/secrets/secrets.py
@@ -0,0 +1,12 @@
+import os
+
+## YouTube API ##
+# https://console.developers.google.com/
+yt_api_key = os.environ["yt_api_key"]
+
+## PostgresDB ##
+db_name = os.environ["db_name"]
+db_user = os.environ["db_user"]
+db_password = os.environ["db_password"]
+db_host = os.environ["db_host"]
+db_port = os.environ["db_port"]
