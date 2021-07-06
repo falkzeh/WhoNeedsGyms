cf0bfa552b4a6637e4cc705096f354fa6fe12775
falkzeh <mail@falkzeh.io>
2021-07-06 23:06:49 +0100

    update secrets for streamlit share

--- src/functions/secrets/secrets.py
+++ src/functions/secrets/secrets.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 
 ## YouTube API ##
 # https://console.developers.google.com/
@@ -10,3 +11,11 @@ db_user = os.environ["db_user"]
 db_password = os.environ["db_password"]
 db_host = os.environ["db_host"]
 db_port = os.environ["db_port"]
+
+## Added for Streamlit Cloud
+yt_api_key = st.secrets["yt_api_key"]
+db_name = st.secrets["db_name"]
+db_user = st.secrets["db_user"]
+db_password = st.secrets["db_password"]
+db_host = st.secrets["db_host"]
+db_port = st.secrets["db_port"]
